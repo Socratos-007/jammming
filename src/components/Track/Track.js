0a4e0598e8a274d922151f1a65dff4dda1b86c34
Socratos-007 <timurid12@gmail.com>
2025-06-29 04:37:21 +0600

    Implemented adding tracks to playlist

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './Track.css';
 
-function Track({ track }) {
+function Track({ track, onAdd, isRemoval }) {
+  const handleAdd = () => {
+    onAdd(track);
+  };
+
   return (
     <div className="Track">
-      <h3>{track.name}</h3>
-      <p>
-        {track.artist} | {track.album}
-      </p>
+      <div>
+        <h3>{track.name}</h3>
+        <p>
+          {track.artist} | {track.album}
+        </p>
+      </div>
+      <button onClick={handleAdd}>{isRemoval ? '-' : '+'}</button>
     </div>
   );
 }
